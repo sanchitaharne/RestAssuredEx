4f674877c175c99a9677fbe97251786dfb60f4b9
sanchita <sanchita.241@gmail.com>
2022-10-15 17:58:03 +0530

    commit

---
+++ src/main/java/ApiTests.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+import static io.restassured.RestAssured.given;
+public class ApiTests {
+
+    @Test
+    public void getResources(){
+
+        String endpoint = "http://localhost/api_testing/category/read.php";
+        var response = given().when().get(endpoint).then();
+        response.log().body();
+    }
+
+    @Test
+    public void getResource(){
+        String endpoint = "http://localhost/api_testing/category/read_one.php";
+        var response =
+                given().
+                        queryParam("id", 1)
+               .when().
+                        get(endpoint)
+               .then();
+        response.log().body();
+    }
+}
