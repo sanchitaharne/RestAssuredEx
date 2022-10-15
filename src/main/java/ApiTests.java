a944a49ef7a248f659c7f47c24072a6107229e2e
sanchita <sanchita.241@gmail.com>
2022-10-15 23:14:47 +0530

    P

--- src/main/java/ApiTests.java
+++ src/main/java/ApiTests.java
@@ -1,24 +1,76 @@
+import models.Product;
 import org.junit.Test;
+
+import java.util.Locale;
+
 import static io.restassured.RestAssured.given;
 public class ApiTests {
 /* */
     @Test
     public void getResources(){
 
-        String endpoint = "http://localhost/api_testing/category/read.php";
+        String endpoint = "http://localhost:80/api_testing/category/read.php";
         var response = given().when().get(endpoint).then();
         response.log().body();
     }
 
     @Test
     public void getResource(){
-        String endpoint = "http://localhost/api_testing/category/read_one.php";
-        var response =
-                given().
-                        queryParam("id", 1)
-               .when().
-                        get(endpoint)
-               .then();
+
+        String endpoint = "http://localhost:80/api_testing/product/read_one.php";
+        var response = given().queryParam("id", 1).when().get(endpoint).then();
         response.log().body();
     }
+
+   @Test
+    public void createResource(){
+
+        String endpoint = "http://localhost:80/api_testing/product/create.php";
+        String body= "{" +
+                     "\"name\": \"water bottle\", " +
+                     "\"description\": \"This is a blue bottle\", " +
+                     "\"price\": 12, " +
+                     "\"category_id\":3" +
+                     "}";
+
+        var response = given().body(body).when().post(endpoint).then();
+        response.log().body();
+   }
+
+   @Test
+    public void updateResourse(){
+
+        String endpoint = "http://localhost:80/api_testing/product/update.php";
+        String body = "{" +
+                "\"id\" = 1001, " +
+                "\"name\": \"water bottle\", " +
+                "\"price\": 99.00, " +
+                "\"category_id\":3" +
+                "}";
+        var response = given().body(body).when().put(endpoint).then();
+        response.log().body();
+   }
+
+   @Test
+    public void deleteResource(){
+        String endpoint= "http://localhost:80/api_testing/product/delete.php";
+        String body = "{\"id\":1001}";
+        var response = given().body(body).when().delete(endpoint).then();
+        response.log().body();
+
+   }
+   @Test
+    public void createSerializedProduct(){
+
+       String endpoint = "http://localhost:80/api_testing/product/create.php";
+       Product product = new Product(
+         "pencil",
+         "Used for writing texts",
+         2,
+         1
+       );
+
+       var response = given().body(product).when().post(endpoint).then();
+       response.log().body();
+   }
 }
---
+++ src/main/java/models/Product.java
@@ -0,0 +1,70 @@
+package models;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int category_id;
+
+    public Product(){}
+
+//used for post request
+    public Product(String name, String description, double price, int category_id){
+
+        setName(name);
+        setDescription(description);
+        setPrice(price);
+        setCategory_id(category_id);
+    }
+
+    //used for put request
+    public Product(int id, String name, String description, double price, int category_id){
+
+        setId(id);
+        setName(name);
+        setDescription(description);
+        setPrice(price);
+        setCategory_id(category_id);
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+}
